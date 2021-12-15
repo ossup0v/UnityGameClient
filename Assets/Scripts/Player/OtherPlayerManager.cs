f09659f978ac63b00f6fbe49836b5fc7f1010fe1
ossup0v <ossup0v@gmail.com>
2021-12-15 22:28:05 +0300

    add healthManager & fix & rename

--- Assets/Scripts/Player/OtherPlayerManager.cs
+++ Assets/Scripts/Player/OtherPlayerManager.cs
@@ -5,36 +5,39 @@ public class OtherPlayerManager : PlayerManager
 {
     public GameObject HealthbarPrefab;
     public HealthbarScript Healthbar;
+    public MeshRenderer EyesRenderer;
 
     public override void Initialize(int id, string username, WeaponKind currentWeapon)
     {
-        OnDie += PlayerDie;
-        OnRespawn += PlayerRespawn;
-        base.HealthChanged += HealthChanged;
+        PlayerDie += OnPlayerDie;
+        PlayerRespawn += OnPlayerRespawn;
+        healthManager.HealthChanged += OnHealthChanged;
 
-        Healthbar.SetMaxHealth(MaxHealth);
+        Healthbar.SetMaxHealth(healthManager.MaxPlayerHealth);
         base.Initialize(id, username, currentWeapon);
     }
 
-    private void HealthChanged(float hp)
+    private void OnHealthChanged(float hp)
     { 
         Healthbar.SetHealth(hp);
     }
 
-    private void PlayerDie()
+    private void OnPlayerDie()
     {
         HealthbarPrefab.SetActive(false);
+        EyesRenderer.enabled = false;
     }
 
-    private void PlayerRespawn()
+    private void OnPlayerRespawn()
     {
         HealthbarPrefab.SetActive(true);
+        EyesRenderer.enabled = true;
     }
 
     private void OnDestroy()
     {
-        OnDie -= PlayerDie;
-        OnRespawn -= PlayerRespawn;
-        base.HealthChanged -= HealthChanged;
+        PlayerDie -= OnPlayerDie;
+        PlayerRespawn -= OnPlayerRespawn;
+        healthManager.HealthChanged -= OnHealthChanged;
     }
 }
