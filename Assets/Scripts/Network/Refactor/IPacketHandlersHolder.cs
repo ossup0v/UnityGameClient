828dac7ab24cf0c3617a4bf7eaba850b9aa1d3ba
enjoythevibes <67597357+enjoythevibes@users.noreply.github.com>
2022-01-07 23:46:21 +0300

    wip refactor

--- Assets/Scripts/Network/Refactor/IPacketHandlersHolder.cs
+++ Assets/Scripts/Network/Refactor/IPacketHandlersHolder.cs
@@ -1,5 +1,4 @@
 public interface IPacketHandlersHolder
 {
-    // T GetPacketHandlerByPacketID<T>(int packetID) where T : class, IPacketHandleable;
     IPacketHandleable GetPacketHandlerByPacketID(int packetID);
 }
